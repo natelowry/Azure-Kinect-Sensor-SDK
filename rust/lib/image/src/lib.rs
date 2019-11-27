759d0f0717d4aa08c90714ea3ee37358d50a541f
Brent Allen <ballen@brentallen.com>
2019-11-27 09:26:51 -0800

    Oxidize the image type

--- rust/lib/image/src/lib.rs
+++ rust/lib/image/src/lib.rs
@@ -8,22 +8,19 @@ mod tests {
 
         let mut img2 = Image::new(ImageFormat::default, 100, 100, 100, Box::new(vec![0; 40]));
 
-        img1.set_iso_speed(100);
+        *img1.iso_speed_mut() = 100;
 
         {
-            let mut buffer = img1.buffer().unwrap();
+            let mut buffer = img1.buffer_mut();
 
-            let mut b = (**buffer).as_mut();
-
-            b[0] = 3;
+            buffer[0] = 3;
         }
 
         {
-            let mut buffer = img1.buffer().unwrap();
+            let mut buffer = img1.buffer_mut();
 
-            let b = (**buffer).as_mut();
 
-            assert_eq!(b[0], 3);
+            assert_eq!(buffer[0], 3);
         }
     }
 }
@@ -43,18 +40,15 @@ struct MutableImageState {
     iso_speed: u32,
 }
 
-struct ImageState {
-    mutable: RwLock<MutableImageState>,
+pub struct Image {
+    mutable: MutableImageState,
     format: ImageFormat,
     width_pixels: i32,
     height_pixels: i32,
     stride_bytes: i32,
-    buffer: Mutex<Box<dyn AsMut<[u8]>>>,
+    buffer: Box<dyn AsMut<[u8]>>,
 }
 
-pub struct Image {
-    state: Arc<ImageState>,
-}
 
 impl Image {
     pub fn new(
@@ -65,66 +59,67 @@ impl Image {
         buffer: Box<dyn AsMut<[u8]>>,
     ) -> Self {
         Image {
-            state: Arc::new(ImageState {
-                mutable: RwLock::new(MutableImageState {
+                mutable: MutableImageState {
                     device_timestamp_usec: 0,
                     system_timestamp_nsec: 0,
                     exposure_time_usec: 0,
                     white_balance: 0,
                     iso_speed: 0,
-                }),
+                },
                 format: format,
                 width_pixels: width_pixels,
                 height_pixels: height_pixels,
                 stride_bytes: stride_bytes,
-                buffer: Mutex::new(buffer),
-            }),
+                buffer: buffer,
         }
     }
 
     fn format(&self) -> ImageFormat {
-        self.state.format
+        self.format
     }
     fn width_pixels(&self) -> i32 {
-        self.state.width_pixels
+        self.width_pixels
     }
     fn height_pixels(&self) -> i32 {
-        self.state.height_pixels
+        self.height_pixels
     }
     fn stride_bytes(&self) -> i32 {
-        self.state.stride_bytes
+        self.stride_bytes
     }
-    fn buffer(&mut self) -> std::sync::LockResult<std::sync::MutexGuard<Box<dyn AsMut<[u8]>>>> {
-        self.state.buffer.lock()
+    
+    fn buffer_mut(&mut self) -> &mut [u8] {
+        (*self.buffer).as_mut()
     }
-    fn device_timestamp_usec(&self) -> u64 {
-        self.state.mutable.read().unwrap().device_timestamp_usec
+
+    fn device_timestamp_usec(&self) -> &u64 {
+        &self.mutable.device_timestamp_usec
     }
-    fn system_timestamp_nsec(&self) -> u64 {
-        self.state.mutable.read().unwrap().system_timestamp_nsec
+    fn system_timestamp_nsec(&self) -> &u64 {
+        &self.mutable.system_timestamp_nsec
     }
-    fn exposure_time_usec(&self) -> u64 {
-        self.state.mutable.read().unwrap().exposure_time_usec
+    fn exposure_time_usec(&self) -> &u64 {
+        &self.mutable.exposure_time_usec
     }
-    fn white_balance(&self) -> u32 {
-        self.state.mutable.read().unwrap().white_balance
+    fn white_balance(&self) -> &u32 {
+        &self.mutable.white_balance
     }
-    fn iso_speed(&self) -> u32 {
-        self.state.mutable.read().unwrap().iso_speed
+    fn iso_speed(&self) -> &u32 {
+        &self.mutable.iso_speed
+    } 
+
+    fn device_timestamp_usec_mut(&mut self) -> &mut u64 {
+        &mut self.mutable.device_timestamp_usec
     }
-    fn set_device_timestamp_usec(&mut self, value: u64) {
-        self.state.mutable.write().unwrap().device_timestamp_usec = value;
+    fn system_timestamp_nsec_mut(&mut self) -> &mut u64 {
+        &mut self.mutable.system_timestamp_nsec
     }
-    fn set_system_timestamp_nsec(&mut self, value: u64) {
-        self.state.mutable.write().unwrap().system_timestamp_nsec = value;
+    fn exposure_time_usec_mut(&mut self) -> &mut u64 {
+        &mut self.mutable.exposure_time_usec
     }
-    fn set_exposure_time_usec(&mut self, value: u64) {
-        self.state.mutable.write().unwrap().exposure_time_usec = value;
+    fn white_balance_mut(&mut self) -> &mut u32 {
+        &mut self.mutable.white_balance
     }
-    fn set_white_balance(&mut self, value: u32) {
-        self.state.mutable.write().unwrap().white_balance = value;
-    }
-    fn set_iso_speed(&mut self, value: u32) {
-        self.state.mutable.write().unwrap().iso_speed = value;
-    }
+    fn iso_speed_mut(&mut self) -> &mut u32 {
+        &mut self.mutable.iso_speed
+    }    
 }
